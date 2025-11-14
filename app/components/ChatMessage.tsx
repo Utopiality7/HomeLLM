48877dfb5e6776c439038bdf15bac63ec10744a5
utopiality7 <akirameruna7315@gmail.com>
2025-11-14 07:02:31 -0500

    refactored ChatMessage

--- app/components/ChatMessage.tsx
+++ app/components/ChatMessage.tsx
@@ -9,20 +9,27 @@ export default function ChatMessage({ msg }: { msg: Message }) {
 
 	return (
 		<>
+			{/* Message title if user none, if assistant AI */}
 			<p
 				className={`${isUserMsg ? 'self-end' : ''} capitalize text-lightTextSecondary dark:text-darkTextSecondary`}
 			>
 				{msg.role === 'user' ? '' : 'AI'}
 			</p>
-			<Markdown
-				className={`${
-					isUserMsg ? 'userMessage' : ''
-				} relative min-w-32 rounded-2xl p-4 leading-8`}
-				rehypePlugins={[rehypeHighlight]}
-				remarkPlugins={[remarkGfm]}
-			>
-				{msg.content}
-			</Markdown>
+			{/* if assistant render markdown */}
+			{msg.role === 'user' ? (
+				<p className="userMessage relative min-w-32 rounded-2xl p-4 leading-8">
+					{msg.content}
+				</p>
+			) : (
+				<Markdown
+					className={`relative min-w-32 rounded-2xl p-4 leading-8`}
+					rehypePlugins={[rehypeHighlight]}
+					remarkPlugins={[remarkGfm]}
+				>
+					{msg.content}
+				</Markdown>
+			)}
+
 			<p
 				className={`text-xs font-light text-lightTextSecondary opacity-90 dark:text-darkTextSecondary ${isUserMsg ? 'self-end' : ''}`}
 			>
